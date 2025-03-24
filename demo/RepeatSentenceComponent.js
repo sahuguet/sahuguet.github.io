d147a5cd776d3a4003a47a9717c6a0eeaa595a90
Arnaud Sahuguet <sahuguet@users.noreply.github.com>
2025-03-24 18:54:44 -0400

    adding russian

--- demo/RepeatSentenceComponent.js
+++ demo/RepeatSentenceComponent.js
@@ -1,34 +1,60 @@
 class RepeatSentenceComponent extends HTMLElement {
+    static get observedAttributes() {
+        return ['lang', 'voice'];
+    }
+
     constructor() {
         super();
 
         // Attach Shadow DOM (optional)
         const shadow = this.attachShadow({ mode: "open" });
         
-
         const style = document.createElement('style');
         const slot = document.createElement('slot');
         slot.style.display = 'none';
-        const div =    document.createElement('div');
+        const div = document.createElement('div');
+
         div.setAttribute('class', 'repeat-sentence');
         div.style.display = 'flex';
-        const speak = document.createElement('ez-speak');
-        const rec = document.createElement('speech-recognition');
+        this.speak = document.createElement('ez-speak');
+        this.rec = document.createElement('speech-recognition');
+
+        // Set initial attributes if present
+        if (this.hasAttribute('lang')) {
+            const lang = this.getAttribute('lang');
+            this.speak.setAttribute('lang', lang);
+        }
+
+        if (this.hasAttribute('voice')) {
+            const voice = this.getAttribute('voice');
+            this.speak.setAttribute('voice', voice);
+        }
 
         shadow.appendChild(style);
-        div.appendChild(speak);
-        div.appendChild(rec);
+        div.appendChild(this.speak);
+        div.appendChild(this.rec);
         shadow.appendChild(div);
         shadow.appendChild(slot);
         
         slot.addEventListener('slotchange', () => {
             const sentence = slot.assignedNodes().map(node => node.textContent).join('').trim();
-            rec.setAttribute('sentence', sentence);
-            speak.textContent = sentence;
-          });
-
+            this.rec.setAttribute('sentence', sentence);
+            this.speak.textContent = sentence;
+        });
     }
 
+    attributeChangedCallback(name, oldValue, newValue) {
+        if (oldValue === newValue) return;
+
+        switch (name) {
+            case 'lang':
+                this.speak.setAttribute('lang', newValue);
+                break;
+            case 'voice':
+                this.speak.setAttribute('voice', newValue);
+                break;
+        }
+    }
 }
 
 customElements.define("repeat-sentence", RepeatSentenceComponent);
